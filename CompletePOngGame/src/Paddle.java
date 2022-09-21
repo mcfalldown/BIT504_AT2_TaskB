25e716e00ee41843e3311f2d649835e3632ac695
mcfalldown <duncgut@gmail.com>
2022-09-22 00:49:06 +1200

    update of paddle colour

--- CompletePOngGame/src/Paddle.java
+++ CompletePOngGame/src/Paddle.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 public class Paddle extends Sprite {
 	private static final int PADDLE_WIDTH = 10;
 	private static final int PADDLE_HEIGHT = 100;
-	private static final Color PADDLE_COLOUR = Color.WHITE;
+	private static final Color PADDLE_COLOUR = Color.BLACK;
 	public static final int DISTANCE_FROM_EDGE = 40;
 	
 	public Paddle(Player player, int panelWidth, int panelHeight) {
